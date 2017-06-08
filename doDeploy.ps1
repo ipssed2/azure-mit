22b5875db5a3e310da82e8b895b31ac4bdce848b
Mark Riordan <mriordan@ipswitch.com>
2017-06-08 09:18:58 -0700

    Improve doDeploy.ps1 to zero-pad resource group names, making them easier to read.

--- doDeploy.ps1
+++ doDeploy.ps1
@@ -1,8 +1,9 @@
 $mynow = Get-Date
-$id = ($mynow.Year.ToString() + $mynow.Month.ToString() + $mynow.Day.ToString() + $mynow.Hour.ToString() + $mynow.Minute.ToString() + $mynow.Second.ToString())
+$id = ($mynow.Year.ToString("0000") + $mynow.Month.ToString("00") + $mynow.Day.ToString("00") + $mynow.Hour.ToString("00") + $mynow.Minute.ToString("00") + $mynow.Second.ToString("00"))
 $RGName = "R" + $id
 $DeployName = "D" + $id
+echo "Resource group will be " $RGName
 New-AzureRmResourceGroup -Name $RGName -Location "WestUS2"
-$mynow
+Get-Date
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -Name $DeployName -Mode Incremental -TemplateParameterFile .\bootParameters.json -TemplateFile .\mainTemplate.json
 Get-Date
