1ee6c0ef30e648b5fb6489342d3deb14309879e3
Mark Riordan <mriordan@ipswitch.com>
2017-06-07 19:57:18 -0700

    Rework doDeploy.ps1 to not rely on a batch file. Change the image reference in mainTemplate.json.

--- doDeploy.ps1
+++ doDeploy.ps1
@@ -1,2 +1,8 @@
-Start-Process "cmd.exe" "/c MkDeployScript.bat" 
-.\doit.ps1
+$mynow = Get-Date
+$id = ($mynow.Year.ToString() + $mynow.Month.ToString() + $mynow.Day.ToString() + $mynow.Hour.ToString() + $mynow.Minute.ToString() + $mynow.Second.ToString())
+$RGName = "R" + $id
+$DeployName = "D" + $id
+New-AzureRmResourceGroup -Name $RGName -Location "WestUS2"
+$mynow
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -Name $DeployName -Mode Incremental -TemplateParameterFile .\bootParameters.json -TemplateFile .\mainTemplate.json
+Get-Date
