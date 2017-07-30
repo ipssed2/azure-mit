17b102f00467537126cd901581a655c305ac5f67
Mark Riordan <mriordan@ipswitch.com>
2017-07-30 13:41:34 -0700

    Add Location parameter to doDeploy2.ps1

--- doDeploy2.ps1
+++ doDeploy2.ps1
@@ -1,11 +1,12 @@
+param([string]$Location='westus2')
 $mynow = Get-Date
 $id = ($mynow.Year.ToString("0000") + $mynow.Month.ToString("00") + $mynow.Day.ToString("00") + $mynow.Hour.ToString("00") + $mynow.Minute.ToString("00"))
-$RGName = "RGS" + $id
-$DeployName = "D" + $id
-echo "Resource group will be " $RGName
+$RGName = "MRR" + $id
+$DeployName = "DMRR" + $id
+echo ("Resource group will be " + $RGName + " in " + $Location)
 Get-Date;
-New-AzureRmResourceGroup -Name $RGName -Location "WestUS2";
+New-AzureRmResourceGroup -Name $RGName -Location $Location;
 Get-Date;
-New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -Name $DeployName -Mode Incremental -TemplateParameterFile .\myParametersSendGrid.json -TemplateFile .\mainTemplate2.json;
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -Name $DeployName -Mode Incremental -TemplateParameterFile .\myParametersSendGrid.json -TemplateFile .\mainTemplate.json;
 Get-Date;
 (Get-AzureRmPublicIpAddress -ResourceGroupName $RGName).IPAddress
